659fbd3fd5a267096451630e00ee605bb4d779bf
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2022-01-19 11:01:14 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using RestSharp;
+
+namespace PspdfkitApiDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new RestClient("https://api.pspdfkit.com/build");
+
+			var request = new RestRequest(Method.POST)
+			  .AddHeader("Authorization", "Bearer {YOUR_API_KEY}") // Replace {YOUR_API_KEY} with your API key.")
+			  .AddFile("index.html", "index.html")
+			  .AddFile("style.css", "style.css")
+			  .AddFile("Inter-Regular.ttf", "Inter-Regular.ttf")
+			  .AddFile("Inter-Medium.ttf", "Inter-Medium.ttf")
+			  .AddFile("Inter-Bold.ttf", "Inter-Bold.ttf")
+			  .AddFile("SpaceMono-Regular.ttf", "SpaceMono-Regular.ttf")
+			  .AddFile("logo.svg", "logo.svg")
+			  .AddParameter("instructions", new JsonObject
+			  {
+				  ["parts"] = new JsonArray
+				{
+			new JsonObject
+			{
+			  ["html"] = "index.html",
+			  ["assets"] = new JsonArray
+			  {
+				"style.css",
+				"Inter-Regular.ttf",
+				"Inter-Medium.ttf",
+				"Inter-Bold.ttf",
+				"SpaceMono-Regular.ttf",
+				"logo.svg"
+			  }
+			}
+				}
+			  }.ToString());
+
+			request.AdvancedResponseWriter = (responseStream, response) =>
+			{
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					using (responseStream)
+					{
+						using var outputFileWriter = File.OpenWrite("result.pdf");
+						responseStream.CopyTo(outputFileWriter);
+					}
+				}
+				else
+				{
+					var responseStreamReader = new StreamReader(responseStream);
+					Console.Write(responseStreamReader.ReadToEnd());
+				}
+			};
+
+			client.Execute(request);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+## PDF Generation with C#
+
+This is a PDF Generator API example that creates a receipt PDF from HTML with C#. Replace the `your_api_key_here` placeholder with your live API key from https://dashboard.pspdfkit.com/api_keys/ in `index.js`.
+
+## Installation
+
+```bash
+# Install RestSharp
+dotnet add package RestSharp -v 106.15.0
+
+# Run the example
+dotnet run
+```
+
+The resulting PDF will be saved in the `result.pdf` file.
+
+
+## Contributing
+
+Please ensure
+[you have signed our CLA](https://pspdfkit.com/guides/web/current/miscellaneous/contributing/) so that we can
+accept your contributions.
